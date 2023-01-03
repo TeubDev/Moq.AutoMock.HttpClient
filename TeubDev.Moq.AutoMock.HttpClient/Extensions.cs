e2d22d637f2950b243239a49d1b5b10b3b83154e
Michael Teubner <micteu@pm.me>
2023-01-02 20:43:51 -0600

    Dropped to .NET Standard 2.1. Bye bye, nice features.

--- TeubDev.Moq.AutoMock.HttpClient/Extensions.cs
+++ TeubDev.Moq.AutoMock.HttpClient/Extensions.cs
@@ -1,19 +1,23 @@
 ﻿using Moq.AutoMock;
+using System;
+using System.Net.Http;
 
-namespace TeubDev.Moq.AutoMock;
-
-/// <summary>
-/// Contains extension methods.
-/// </summary>
-public static class Extensions
+namespace TeubDev.Moq.AutoMock
 {
     /// <summary>
-    /// Gets an <see cref="HttpClientMock"/> for use in unit tests.
+    /// Contains extension methods.
     /// </summary>
-    /// <param name="autoMocker">The AutoMocker used for getting mocks.</param>
-    /// <param name="baseAddress">The base address that should be passed into a created <see cref="HttpClient"/></param>
-    /// <remarks>
-    /// The baseAddress parameter is needed if the unit test uses a relative URI instead of absolute.
-    /// </remarks>
-    public static HttpClientMock GetHttpClientMock(this AutoMocker autoMocker, Uri? baseAddress = null) => new (autoMocker, baseAddress);
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets an <see cref="HttpClientMock"/> for use in unit tests.
+        /// </summary>
+        /// <param name="autoMocker">The AutoMocker used for getting mocks.</param>
+        /// <param name="baseAddress">The base address that should be passed into a created <see cref="HttpClient"/></param>
+        /// <remarks>
+        /// The baseAddress parameter is needed if the unit test uses a relative URI instead of absolute.
+        /// </remarks>
+        public static HttpClientMock GetHttpClientMock(this AutoMocker autoMocker, Uri? baseAddress = null) =>
+            new HttpClientMock(autoMocker, baseAddress);
+    }
 }
--- TeubDev.Moq.AutoMock.HttpClient/HttpClientMock.cs
+++ TeubDev.Moq.AutoMock.HttpClient/HttpClientMock.cs
@@ -2,52 +2,57 @@
 using Moq.AutoMock;
 using Moq.Language.Flow;
 using Moq.Protected;
+using System;
 using System.Linq.Expressions;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace TeubDev.Moq.AutoMock;
-
-/// <summary>
-/// A fake little wrapper that makes it a bit easier to mock out HttpClients.
-/// </summary>
-public class HttpClientMock
+namespace TeubDev.Moq.AutoMock
 {
-    private readonly Mock<HttpMessageHandler> handlerMock;
-
-    internal HttpClientMock(AutoMocker mocker, Uri? baseAddress)
+    /// <summary>
+    /// A fake little wrapper that makes it a bit easier to mock out HttpClients.
+    /// </summary>
+    public class HttpClientMock
     {
-        handlerMock = mocker.GetMock<HttpMessageHandler>();
-        var httpClient = mocker.CreateInstance<HttpClient>();
-        if (baseAddress != null)
+        private readonly Mock<HttpMessageHandler> handlerMock;
+
+        internal HttpClientMock(AutoMocker mocker, Uri? baseAddress)
         {
-            httpClient.BaseAddress = baseAddress;
+            handlerMock = mocker.GetMock<HttpMessageHandler>();
+            var httpClient = mocker.CreateInstance<HttpClient>();
+            if (baseAddress != null)
+            {
+                httpClient.BaseAddress = baseAddress;
+            }
+            mocker.Use(httpClient);
         }
-        mocker.Use(httpClient);
+
+        /// <summary>
+        /// Specifies a setup for an invocation on the SendAsync method of an underlying HttpClientHandler
+        /// </summary>
+        /// <param name="requestMatcher">A function for things to check about a request to verify it matches the setup.</param>
+        public ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupRequest(
+            Expression<Func<HttpRequestMessage, bool>> requestMatcher) =>
+            handlerMock.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.Is(requestMatcher),
+                    ItExpr.IsAny<CancellationToken>());
+
+        /// <summary>
+        /// Verifies that the matching request was called the given number of times.
+        /// </summary>
+        public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher, Times times) =>
+            handlerMock.Protected()
+                .Verify<Task<HttpResponseMessage>>("SendAsync", times, ItExpr.Is(requestMatcher),
+                    ItExpr.IsAny<CancellationToken>());
+
+        /// <inheritdoc cref="Verify(Expression{Func{HttpRequestMessage, bool}}, Times)"/>
+        public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher, Func<Times> times) =>
+            Verify(requestMatcher, times());
+
+        /// <inheritdoc cref="Verify(Expression{Func{HttpRequestMessage, bool}}, Times)"/>
+        public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher) =>
+            Verify(requestMatcher, Times.Once());
+
     }
-
-    /// <summary>
-    /// Specifies a setup for an invocation on the SendAsync method of an underlying HttpClientHandler
-    /// </summary>
-    /// <param name="requestMatcher">A function for things to check about a request to verify it matches the setup.</param>
-    public ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupRequest(
-        Expression<Func<HttpRequestMessage, bool>> requestMatcher) =>
-        handlerMock.Protected()
-            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.Is(requestMatcher),
-                ItExpr.IsAny<CancellationToken>());
-
-    /// <summary>
-    /// Verifies that the matching request was called the given number of times.
-    /// </summary>
-    public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher, Times times) =>
-        handlerMock.Protected()
-            .Verify<Task<HttpResponseMessage>>("SendAsync", times, ItExpr.Is(requestMatcher),
-                ItExpr.IsAny<CancellationToken>());
-
-    /// <inheritdoc cref="Verify(Expression{Func{HttpRequestMessage, bool}}, Times)"/>
-    public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher, Func<Times> times) =>
-        Verify(requestMatcher, times());
-
-    /// <inheritdoc cref="Verify(Expression{Func{HttpRequestMessage, bool}}, Times)"/>
-    public void Verify(Expression<Func<HttpRequestMessage, bool>> requestMatcher) =>
-        Verify(requestMatcher, Times.Once());
-
 }
